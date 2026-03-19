a3965700bef4da4c3291e79ecbbaeabe03ec51a4
ryoworks4 <yahaha0411ryo@gmail.com>
2026-03-19 17:58:12 +0900

    fix: CORS設定を更新

--- api/chat.js
+++ api/chat.js
@@ -61,7 +61,7 @@ function checkRateLimit(ip) {
 
 export default async function handler(req, res) {
     // CORS
-    res.setHeader('Access-Control-Allow-Origin', 'https://ryoworks4.github.io');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
